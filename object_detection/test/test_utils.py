b8881c00a80c4caa511b7b4a50bdeb4e33ceff66
Vlad Shmyhlo <aion.stu@gmail.com>
2020-06-24 17:42:29 +0300

    Update

--- object_detection/test/test_utils.py
+++ object_detection/test/test_utils.py
@@ -7,22 +7,17 @@ def test_per_class_nms():
     boxes = torch.tensor([
         [0, 0, 1, 1],
         [0, 0, 1, 1],
+        [0, 0, 0.5, 0.5],
     ], dtype=torch.float)
-    scores = torch.ones(2, dtype=torch.float)
-    class_ids = torch.tensor([
-        0,
-        0,
-    ], dtype=torch.long)
+    scores = torch.ones(boxes.size(0), dtype=torch.float)
+    class_ids = torch.tensor([0, 0, 0], dtype=torch.long)
 
     actual = per_class_nms(boxes, scores, class_ids, iou_threshold=0.5)
-    expected = torch.tensor([0], dtype=torch.long)
+    expected = torch.tensor([0, 2], dtype=torch.long)
     assert torch.allclose(actual, expected)
 
-    class_ids = torch.tensor([
-        0,
-        1,
-    ], dtype=torch.long)
+    class_ids = torch.tensor([0, 1, 1], dtype=torch.long)
 
     actual = per_class_nms(boxes, scores, class_ids, iou_threshold=0.5)
-    expected = torch.tensor([0, 1], dtype=torch.long)
+    expected = torch.tensor([0, 1, 2], dtype=torch.long)
     assert torch.allclose(actual, expected)
