90df1ffe44731a0a9259edcdded41108b366fd8c
Vlad Shmyhlo <vlad.shmyhlo@revolut.com>
2021-10-08 09:52:47 +0100

    Update

--- vae/train.py
+++ vae/train.py
@@ -5,11 +5,16 @@ import os
 import torch
 import torch.nn.functional as F
 import torch.utils.data
-from dataset import Dataset
+import torchvision.transforms as T
+from all_the_tools.meters import Mean
+
+# from dataset import Dataset
 from tensorboardX import SummaryWriter
-from ticpfptp.format import args_to_string
-from ticpfptp.metrics import Mean
-from ticpfptp.torch import fix_seed
+from torchvision.datasets import MNIST
+
+# from ticpfptp.format import args_to_string
+# from ticpfptp.metrics import Mean
+# from ticpfptp.torch import fix_seed
 from tqdm import tqdm
 
 import utils
@@ -25,7 +30,7 @@ def build_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--experiment-path", type=str, default="./tf_log")
     parser.add_argument("--restore-path", type=str)
-    parser.add_argument("--dataset-path", type=str, default="./data")
+    parser.add_argument("--dataset-path", type=str, default="./data/mnist")
     parser.add_argument("--learning-rate", type=float, default=5e-5)
     parser.add_argument("--model-size", type=int, default=32)
     parser.add_argument("--latent-size", type=int, default=128)
@@ -39,56 +44,70 @@ def build_parser():
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    args = build_parser().parse_args()
-    logging.info(args_to_string(args))
-    fix_seed(args.seed)
+    config = build_parser().parse_args()
+    # logging.info(args_to_string(config))
+    # fix_seed(config.seed)
+
+    transform = T.Compose([T.ToTensor(), T.Normalize([0.5], [0.5]),])
 
     data_loader = torch.utils.data.DataLoader(
-        Dataset(args.dataset_path),
-        batch_size=args.batch_size,
+        MNIST(config.dataset_path, download=True, transform=transform),
+        batch_size=config.batch_size,
         shuffle=True,
         num_workers=os.cpu_count(),
         drop_last=True,
     )
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    encoder = Encoder(args.model_size, args.latent_size)
-    decoder = Decoder(args.model_size, args.latent_size)
+    encoder = Encoder(config.model_size, config.latent_size)
+    decoder = Decoder(config.model_size, config.latent_size)
     encoder.to(device)
     decoder.to(device)
 
     opt = torch.optim.Adam(
-        list(encoder.parameters()) + list(decoder.parameters()), lr=args.learning_rate
+        list(encoder.parameters()) + list(decoder.parameters()), lr=config.learning_rate
     )
-    noise_dist = torch.distributions.Normal(0, 1)
 
-    writer = SummaryWriter(args.experiment_path)
+    writer = SummaryWriter(config.experiment_path)
     metrics = {"loss": Mean()}
 
-    for epoch in range(args.epochs):
+    for epoch in range(config.epochs):
         encoder.train()
         decoder.train()
-        for real, _ in tqdm(data_loader, desc="epoch {} training".format(epoch)):
-            real = real.to(device)
+        for x, _ in tqdm(data_loader, desc="epoch {} training".format(epoch)):
+            x = x.to(device)
 
-            mean, log_var = encoder(real)
-            latent = noise_dist.sample((args.batch_size, args.latent_size)).to(device)
-            latent = mean + latent * torch.exp(log_var / 2)
-            fake = decoder(latent)
+            dist_z = encoder(x)
+            z = dist_z.rsample()
+            dist_x = decoder(z)
+            loss = compute_loss(dist_z, z, dist_x, x)
 
-            # TODO: loss (reconstruction, summing, mean)
-            mse = F.mse_loss(input=fake, target=real)
-            kld = -0.5 * (1 + log_var - mean ** 2 - log_var.exp()).sum(-1)
-            loss = mse.mean() + kld.mean()
-            metrics["loss"].update(loss.data.cpu().numpy())
+            print(loss.shape)
 
             opt.zero_grad()
             loss.mean().backward()
             opt.step()
 
+            metrics["loss"].update(loss.detach())
+
         writer.add_scalar("loss", metrics["loss"].compute_and_reset(), global_step=epoch)
-        writer.add_image("real", utils.make_grid((real + 1) / 2), global_step=epoch)
-        writer.add_image("fake", utils.make_grid((fake + 1) / 2), global_step=epoch)
+        writer.add_image("x", utils.make_grid(x * 0.5 + 0.5), global_step=epoch)
+        # writer.add_image("x_hat", utils.make_grid((fake + 1) / 2), global_step=epoch)
+
+
+def compute_loss(dist_qzgx, z, dist_pxgz, x):
+    dist_pz = torch.distributions.Normal(
+        torch.zeros_like(dist_qzgx.mean), torch.ones_like(dist_qzgx.scale),
+    )
+
+    log_qzgx = dist_qzgx.log_prob(z).sum(1)
+    log_pz = dist_pz.log_prob(z).sum(1)
+    log_pxgz = dist_pxgz.log_prob(x).sum((1, 2, 3))
+
+    kl = log_qzgx - log_pz
+    loss = kl - log_pxgz
+
+    return loss
 
 
 if __name__ == "__main__":
